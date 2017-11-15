ef8a4d6fbeb318cfd8e1083e1293a8f7b2861524
Matt Kim <mingyuya@gmail.com>
2017-11-16 07:55:55 +0900

    Update particle_filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -168,9 +168,9 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 }
 
 void ParticleFilter::resample() {
-	// TODO: Resample particles with replacement with probability proportional to their weight. 
-	// NOTE: You may find std::discrete_distribution helpful here.
-	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+	// - Resample particles with replacement with probability proportional to their weight. 
+	// - Reference for std::discrete_distribution
+	//    : http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
   vector<Particle> resampled_particles;
 
